5a81b2745238f2cfd2fc0dca296a5b2a1b4db4de
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2026-05-21 12:44:40 -0400

    made options screen

--- core/src/main/java/io/github/longfall/OptionsScreen.java
+++ core/src/main/java/io/github/longfall/OptionsScreen.java
@@ -11,18 +11,12 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.utils.Align;
-import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.kotcrab.vis.ui.widget.VisLabel;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import dev.lyze.flexbox.FlexBox;
 import io.github.orioncraftmc.meditate.YogaNode;
@@ -30,8 +24,7 @@ import io.github.orioncraftmc.meditate.enums.YogaEdge;
 import io.github.orioncraftmc.meditate.enums.YogaFlexDirection;
 import io.github.orioncraftmc.meditate.enums.YogaWrap;
 
-/** First screen of the application. Displayed after the application is created. */
-public class FirstScreen implements Screen {
+public class OptionsScreen implements Screen {
 
     private OrthographicCamera camera;
     private Viewport viewport;
@@ -42,23 +35,26 @@ public class FirstScreen implements Screen {
 
     Stage stage = new Stage(new ScreenViewport());
 
-    FlexBox titleFlexBox = new FlexBox();
+    Skin CurrentSkin = new Skin(Gdx.files.internal("uiskin.json"));
 
     FlexBox optionsFlexBox = new FlexBox();
 
-    YogaNode playButtonNode;
-    TextButton playButton;
-    
-    YogaNode optionsButtonNode;
-    TextButton optionsButton;
 
-    YogaNode quitButtonNode;
-    TextButton quitButton;
+    YogaNode backButtonNode;
+    TextButton backButton;
 
-    Skin CurrentSkin = new Skin(Gdx.files.internal("uiskin.json"));
+    YogaNode soundButtonNode;
+    TextButton soundButton;
 
-    
-	public FirstScreen(final Main game) {
+
+    public OptionsScreen(Main game) {
+
+        this.game = game;
+
+    }
+
+    @Override
+    public void show() {
 
         InputMultiplexer multiplexer = new InputMultiplexer();
         multiplexer.addProcessor(stage);
@@ -68,21 +64,11 @@ public class FirstScreen implements Screen {
         viewport = new FitViewport(1280, 720, camera); 
         viewport.apply();
 
-		this.game = game;
-
         TextureAtlas atlas = new TextureAtlas("./ui/uiskin.atlas");
-        // System.out.println(System.getProperty("user.dir"));
-        // System.out.flush();
 
         CurrentSkin.addRegions(atlas);
 
         backgroundTexture = new Texture(Gdx.files.internal("ui/graphics/ui/LongfallMainMenuBackground.png"));
-
-        titleFlexBox.setFillParent(true);
-        titleFlexBox.getRoot()
-            .setFlexDirection(YogaFlexDirection.ROW)
-            .setWrap(YogaWrap.WRAP);
-        stage.addActor(titleFlexBox);
 
         optionsFlexBox.setFillParent(true);
         optionsFlexBox.getRoot()
@@ -90,98 +76,57 @@ public class FirstScreen implements Screen {
             .setWrap(YogaWrap.WRAP);
         stage.addActor(optionsFlexBox);
 
-        YogaNode titleParentNode = titleFlexBox.add()
-            .setFlexDirection(YogaFlexDirection.COLUMN)
-            .setBorder(YogaEdge.ALL, 25)
-            .setBackground(CurrentSkin.newDrawable("textfield"))
-            .setMargin(YogaEdge.LEFT, 200)
-            .setMargin(YogaEdge.TOP, 75);
-
-		Label title = new Label("Longfall", CurrentSkin);
-            title.setAlignment(Align.center);
-            title.setColor(Color.BROWN);
-            title.setFontScale(4.0f);
-            titleFlexBox.addAsChild(titleParentNode, title);
-
-
         TextButton.TextButtonStyle defaultStyle = new TextButton.TextButtonStyle();
         defaultStyle.font = CurrentSkin.getFont("default-font");
         defaultStyle.fontColor = Color.BROWN;
         defaultStyle.up = CurrentSkin.newDrawable("button-normal");
         defaultStyle.down = CurrentSkin.newDrawable("button-normal-pressed");
 
-        playButton = new TextButton("Play", defaultStyle);
-        playButton.getLabel().setFontScale(2.5f);
+        backButton = new TextButton("Back", defaultStyle);
+        backButton.getLabel().setFontScale(2.5f);
 
-        playButtonNode = optionsFlexBox.add(playButton)
+        backButtonNode = optionsFlexBox.add(backButton)
             .setFlexDirection(YogaFlexDirection.COLUMN)
             .setBorder(YogaEdge.ALL, 25)
             .setMargin(YogaEdge.LEFT, 50)
-            .setMargin(YogaEdge.TOP, 275)
+            .setMargin(YogaEdge.TOP, 75)
             .setWidth(150)
             .setHeight(75);
 
-        playButton.addListener(playButtonListener);
-        playButton.setTouchable(Touchable.enabled);
+        backButton.addListener(BackButtonListener);
+        backButton.setTouchable(Touchable.enabled);
 
-        optionsButton = new TextButton("Options", defaultStyle);
-        optionsButton.getLabel().setFontScale(2.5f);
+        
 
-        optionsButtonNode = optionsFlexBox.add(optionsButton)
+        soundButton = new TextButton("Sound", defaultStyle);
+        soundButton.getLabel().setFontScale(2.5f);
+
+        soundButtonNode = optionsFlexBox.add(soundButton)
             .setFlexDirection(YogaFlexDirection.COLUMN)
             .setBorder(YogaEdge.ALL, 25)
             .setMargin(YogaEdge.LEFT, 50)
-            .setMargin(YogaEdge.TOP, 275)
-            .setWidth(200)
+            .setMargin(YogaEdge.TOP, 75)
+            .setWidth(150)
             .setHeight(75);
 
-        optionsButton.addListener(optionsButtonListener);
-        optionsButton.setTouchable(Touchable.enabled);
+        soundButton.addListener(soundButtonListener);
+        soundButton.setTouchable(Touchable.enabled);
+    }
 
-        quitButton = new TextButton("Quit", defaultStyle);
-        quitButton.getLabel().setFontScale(2.5f);
-
-        quitButtonNode = optionsFlexBox.add(quitButton)
-            .setFlexDirection(YogaFlexDirection.COLUMN)
-            .setBorder(YogaEdge.ALL, 25)
-            .setMargin(YogaEdge.LEFT, 50)
-            .setMargin(YogaEdge.TOP, 275)
-            .setWidth(200)
-            .setHeight(75);
-
-        quitButton.addListener(quitButtonListener);
-        quitButton.setTouchable(Touchable.enabled);
-
-        //stage.setDebugAll(true);
-	}
-
-    ClickListener playButtonListener = new ClickListener() {
-        @Override
-        public void clicked(InputEvent event, float x, float y) {
-            System.out.println("Play button clicked!");
-        }
-    };
-
-    ClickListener optionsButtonListener = new ClickListener() {
+    ClickListener BackButtonListener = new ClickListener() {
         @Override
         public void clicked(InputEvent event, float x, float y) {
             dispose();
-            game.setScreen(new OptionsScreen(game));
+            game.setScreen(new FirstScreen(game));
         }
     };
-    
-    ClickListener quitButtonListener = new ClickListener() {
+
+    ClickListener soundButtonListener = new ClickListener() {
         @Override
         public void clicked(InputEvent event, float x, float y) {
-            Gdx.app.exit();
+            System.out.println("Sound button clicked!");
         }
     };
-    
-
-    @Override
-    public void show() {
-        // Prepare your screen here.
-    }
 
     @Override
     public void render(float delta) {
@@ -201,28 +146,26 @@ public class FirstScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        // If the window is minimized on a desktop (LWJGL3) platform, width and height are 0, which causes problems.
-        // In that case, we don't resize anything, and wait for the window to be a normal size before updating.
+
         if(width <= 0 || height <= 0) return;
 
-        // Resize your screen here. The parameters represent the new window size.
         viewport.update(width, height, true);
         stage.getViewport().update(width, height, true);
     }
 
     @Override
     public void pause() {
-        // Invoked when your application is paused.
+        // TODO Auto-generated method stub
     }
 
     @Override
     public void resume() {
-        // Invoked when your application is resumed after pause.
+        // TODO Auto-generated method stub
     }
 
     @Override
     public void hide() {
-        // This method is called when another screen replaces this one.
+        // TODO Auto-generated method stub
     }
 
     @Override
